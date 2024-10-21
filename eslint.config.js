9d1b5e1663d9ff915c26fe029c7f82bc796a1196
Chris Marsh <chris.marsh@hedgehoglab.com>
2024-10-21 16:22:40 +0100

    Fighting with webpack

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import jest from "eslint-plugin-jest";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [...compat.extends("airbnb-base"), {
+  plugins: {
+    jest,
+  },
+
+  languageOptions: {
+    ecmaVersion: "latest",
+    globals: {
+      ...jest.environments.globals.globals,
+      import: "readonly",
+    },
+  },
+
+  rules: {
+    "no-underscore-dangle": "off",
+    "no-use-before-define": "off",
+    "no-continue": "off",
+    "no-restricted-syntax": ["off", "ForOfLoop"],
+    "no-restricted-globals": ["off", "isNaN"],
+    "import/prefer-default-export": "off",
+    "import/extensions": "off",
+    "quotes": "double",
+  },
+}];
